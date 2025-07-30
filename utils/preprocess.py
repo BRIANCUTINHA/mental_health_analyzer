f91e557c96a836353b2e9ab34f5c776a2247ee74
BRIANCUTINHA <brian.cutinha@gmail.com>
2025-07-30 19:43:54 +0530

    🐍 Fix: add automatic nltk stopwords download

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,7 +1,20 @@
 import re
+import nltk
+from nltk.corpus import stopwords
+
+# Auto-download stopwords if not already downloaded
+try:
+    stop_words = set(stopwords.words('english'))
+except LookupError:
+    nltk.download('stopwords')
+    stop_words = set(stopwords.words('english'))
 
 def clean_text(text):
     text = text.lower()
-    text = re.sub(r'http\S+|www.\S+', '', text)
-    text = re.sub(r'[^a-z\s]', '', text)
-    return text.strip()
+    text = re.sub(r"http\S+|www\S+|https\S+", '', text, flags=re.MULTILINE)
+    text = re.sub(r'\@w+|\#','', text)
+    text = re.sub(r'[^\w\s]', '', text)
+    text = re.sub(r'\d+', '', text)
+    text = text.strip()
+    text = " ".join([word for word in text.split() if word not in stop_words])
+    return text
